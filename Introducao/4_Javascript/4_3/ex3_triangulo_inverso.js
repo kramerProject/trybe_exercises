c2dc6897931460be5de4fbe12340a8c9bfd6bfa0
kramersilva <kramerscs@gmail.com>
2020-08-31 15:28:56 -0300

    ex3 e 6 alterado

--- Introducao/4_Javascript/4_3/ex3_triangulo_inverso.js
+++ Introducao/4_Javascript/4_3/ex3_triangulo_inverso.js
@@ -9,4 +9,4 @@ function invertedTriangle(number) {
     }
   }
 }
-invertedTriangle(5)
+invertedTriangle(7)
--- Introducao/4_Javascript/4_3/ex6_bonus2.js
+++ Introducao/4_Javascript/4_3/ex6_bonus2.js
@@ -2,12 +2,14 @@ function primo(number) {
   let numberDivisor = []
 
   for (counter = 2 ; counter < number ; counter += 1) {
-    if (number % counter == 0) {
+    if (number % counter == 0 && number > 1) {
       numberDivisor.push(counter)
     }
   }
-  if (numberDivisor.length == 0) {
+  if (numberDivisor.length == 0 && number > 1) {
     console.log(`O número ${number} é primo`)
+  } else {
+    console.log(`O número ${number} não é primo`)
   }
 }
-primo(6)
+primo(5)
