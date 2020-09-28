f8e2bc95b7e904a785e73f3ebbeb8be0e6ccb277
kramersilva <kramerscs@gmail.com>
2020-09-28 13:08:21 -0300

    ParteII - ex4.js

---
+++ Introducao/7_Javascript_Es6_testes/7_2/ParteII/ex4.js
@@ -0,0 +1,28 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const printValues = (lesson) => {
+  const arrayOfValues = Object.values(lesson)
+  for (let i in arrayOfValues) {
+    console.log(arrayOfValues[i])
+  }
+}
+
+printValues(lesson2)
