9b51b3a0ae33d9541c65e9eca975383dc0b82f7f
kramersilva <kramerscs@gmail.com>
2020-10-08 12:29:16 -0300

    ex7

---
+++ Introducao/8_Javascript_Es6_HigherOrder/8_4/ex7_ship.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+
+const ships = [
+    {
+        name: "Titanic",
+        length: 269.1
+    },
+    {
+        name: "Queen Mary 2",
+        length: 1132,
+        measurementUnit: "feet"
+    },
+    {
+        name: "Yamato",
+        length: 256,
+        measurementUnit: "meters"
+    }
+]
+
+// escreva shipLength abaixo
+const shipLength = (ship) => {
+  const {name, length, measurementUnit = 'meters'} = ship
+
+  const message = `${name} is ${length} ${measurementUnit} long`
+
+  return message
+}
+
+assert.strictEqual(shipLength(ships[0]), "Titanic is 269.1 meters long")
+assert.strictEqual(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long")
+assert.strictEqual(shipLength(ships[2]), "Yamato is 256 meters long")
